7268a6d95a3d5164eeaf95cd04610948feec0973
youngsun <5youngsun2@naver.com>
2024-02-20 16:21:09 +0900

    Link 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import "./App.css";
 import MainPage from "./main/index.js";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload/index.js";
+import ProductPage from "./product/index.js";
 
 function App() {
-  return <MainPage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <MainPage />
+        </Route>
+        <Route exact={true} path={"/product/:id"}>
+          <ProductPage />
+        </Route>
+        <Route exact={true} path={"/upload"}>
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
