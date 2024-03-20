d630f8e3f1401057b6e73dc8fbb23ea1b1d44fcb
youngsun <5youngsun2@naver.com>
2024-03-20 17:59:35 +0900

    서버 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,5 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://blackmarket-server.onrender.com"
+    ? //    ? "https://blackmarket-server.onrender.com"
+      "https://good-buttons-play.loca.lt"
     : "http://localhost:8080";
